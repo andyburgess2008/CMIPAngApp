6631916a2e4fe94c34297b02e060eb0fe4104999
Anda Luo <anda.luo@hpe.com>
2016-10-15 04:27:44 +0000

    New version1

--- app.js
+++ app.js
@@ -21,8 +21,22 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
 // start server on the specified port and binding host
+
+app.get('/home', function(req, res){
+	res.send('testing message');
+	
+});
+//start server on the specified port and binding hostnew
+app.listen(appEnv.port, host, function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+/*
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+*/
