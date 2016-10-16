3f5b31145683dd7c90c7ccccf16f22dddc5346ff
Anda.Luo <Anda.Luo>
2016-10-16 11:46:39 -0700

    my updated config files

--- app.js
+++ app.js
@@ -11,16 +11,25 @@ var express = require('express');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
+var path =require('path');
 // create a new express server
 var app = express();
 
+var bodyParser =require('body-parser');
+
+
+//set up configuration
+
+//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+//app.set('views', __dirname+ '/views');
+app.set('views', path.join(__dirname, 'views'));
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
+app.use(bodyParser.json());
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 // start server on the specified port and binding host
