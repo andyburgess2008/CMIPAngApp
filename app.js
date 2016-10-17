4cd2950665174258418c74f8df2ebfb45c3d285d
Anda Luo <anda.luo@hpe.com>
2016-10-17 04:42:05 +0000

    New-V941

--- app.js
+++ app.js
@@ -107,8 +107,13 @@ function ensureAuthenticated(req, res, next) {
                      failureRedirect: '/failure',                        
           })(req,res,next);
         });
- app.get('/hello', ensureAuthenticated, function(req, res) {
-             res.send('Hello, '+ req.user['id'] + '!'); });
+ app.get('/hello', ensureAuthenticated, function(req, res, next) {
+             //res.send('Hello, '+ req.user['id'] + '!'); 
+             res.render('default', {
+             	user: req.user
+             });
+             next();
+   });
 
  //
  app.get('/failure', function(req, res) { 
