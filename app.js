e9061278490d1341ead0751929caa7dac1f5a72e
Anda Luo <anda.luo@hpe.com>
2016-10-22 04:54:07 +0000

    V40

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ app.get('/home', function(req, res){
 */
 app.get('/logout', function(req, res) {
         req.logout();
-  res.redirect('https://cmipsso-ald1016-y10rz7i00p-ck11.iam.ibmcloud.com/idaas/mtfim/sps/idaas/logout');
+  res.redirect('https://cmipsso-ald1016-y10rz7i00p-ck11.iam.ibmcloud.com/idaas/mtfim/sps/idaas/logout/');
       });
 
 
